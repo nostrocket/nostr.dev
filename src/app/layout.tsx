4fb51928727ed9b14f6bb14102c35fb763c7cdac
Matt <git@crooks.com.au>
2025-08-11 16:00:34 +0800

    Implement code changes to enhance functionality and improve performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider>
           {children}
